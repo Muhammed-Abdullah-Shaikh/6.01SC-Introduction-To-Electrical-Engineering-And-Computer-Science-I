32850773a113f6b8040138ba57396a84fbeca48a
Muhammed Abdullah Shaikh <muhammed.shaikh@accoladeelectronics.com>
2023-08-24 17:18:52 +0530

    boundaryBrain.py Working but not for turns

--- Unit 1 Software Engineering/State Machines/designLab02/boundaryBrain.py
+++ Unit 1 Software Engineering/State Machines/designLab02/boundaryBrain.py
@@ -12,21 +12,21 @@ from soar.io import io
 # refer https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Triangle_with_notations_2.svg/220px-Triangle_with_notations_2.svg.png
 # for diagram
 Gamma = math.radians(22.5)
-def get_height(a, b, gamma=Gamma):
+def get_median(a, b, gamma=Gamma):
     c =  math.sqrt(a**2 + b**2 - 2*a*b*math.cos(gamma)) # Cosine rule
-    return (a*b/c)*math.sin(gamma) # sine rule
+    return(1 / 2)*math.sqrt(2*(a**2) + 2*(b**2) - c**2) # Apollonius’s Theorem
 
 # Wall Follower
 class MySMClass(sm.SM):
     def getNextValues(self, state, inp):
         sonars = inp.sonars
-        front_distance = get_height(sonars[3], sonars[4]) if sonars[3] < 3 and sonars[4] < 3 else min(sonars[3], sonars[4])
-        right_distance = get_height(sonars[6], sonars[7]) if sonars[6] < 3 and sonars[7] < 3 else min(sonars[6], sonars[7])
+        front_distance = get_median(sonars[3], sonars[4]) if sonars[3] < 3 and sonars[4] < 3 else min(sonars[3], sonars[4])
+        right_distance = get_median(sonars[6], sonars[7]) if sonars[6] < 3 and sonars[7] < 3 else min(sonars[6], sonars[7])
         if sonars[7] < 0.5 and sonars[7] > 0.3:
             right_distance = sonars[7]
         elif sonars[6] < 0.5 and sonars[6] > 0.3:
             right_distance = sonars[6]
-        # right_distance = min(get_height(sonars[6], sonars[7]), sonars[7])
+        # right_distance = min(get_median(sonars[6], sonars[7]), sonars[7])
         print "Front Distance: ", front_distance
         print "Right Hand Side Distance: ", right_distance
         if front_distance > 0.5:
@@ -60,7 +60,7 @@ def plotSonar(sonarNum):
 
 # this function is called when the brain is (re)loaded
 def setup():
-    robot.gfx = gfx.RobotGraphics(drawSlimeTrail=False, # slime trails
+    robot.gfx = gfx.RobotGraphics(drawSlimeTrail=True, # slime trails
                                   sonarMonitor=False) # sonar monitor widget
     
     # set robot's behavior
